3d6570c39726937a1f1d3c56d9db5232816e2d56
deagantest <48824737+deagantest@users.noreply.github.com>
2019-03-28 21:27:09 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+tmux new-session -d -s avatar-server'cd ripple/avatar-server && ./avatar-server'
+tmux new-session -d -s lets'cd ripple/lets && python3.6 lets.py'
+tmux new-session -d -s bancho'cd ripple/pep.py && python3.6 pep.py'
+tmux new-session -d -s api'cd ripple/rippleapi && ./rippleapi'
+tmux new-session -d -s hanayo'cd ripple/hanayo && ./hanayo'
+TMUX HAS BEEN STARTED
